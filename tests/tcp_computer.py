e1c2d4b1f98d1fb779810a8f87c93b165e7376f5
BirB.yeah <1635036180@qq.com>
2022-11-17 20:04:06 +0800

    关于TCP连接的测试文件

---
+++ tests/tcp_computer.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import sys
+
+class TCP_Computer:
+    """
+    I suppose that upper is the computer, 
+    and it is server.
+    """
+    def __init__(self, ip, port) -> None:
+        self.ip = ip
+        self.port = port
+        self.integrated_addr = (self.ip, self.port) # Attention! This is for computer itself!
+        self.thread_receive = threading.Thread(None, self.receive, "receive")
+    
+    def connect(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.bind(self.integrated_addr)
+        self.s.listen(1)
+        self.client, self.client_addr = self.s.accept()
+        print(f"Connected successfully! The address and ip for the client is {self.client_addr[0]} and {self.client_addr[1]}.")
+        self.thread_receive.start()
+        
+    def receive(self):
+        while True:
+            recv_data = self.client.recv(1024).decode("utf-8")
+            if recv_data == "Exit":
+               self.deliver("OK, trying to close this connection...")
+               self.client.close()
+               self.s.close()
+               sys.exit(0)
+            else:
+                print(f"Get:{recv_data}.")
+                self.deliver(f"Server Get:{recv_data}.")
+               
+    def deliver(self,
+                context:str):
+         self.client.send(context.encode("utf-8"))
+    
+if __name__ == "__main__":
+    tcp_computer = TCP_Computer("", 5199)
+    tcp_computer.connect()
---
+++ tests/tcp_drone.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+import sys
+
+class TCP_Drone:
+    """
+    I suppose that down is the drone, 
+    and it is client.
+    """
+    def __init__(self, ip, port) -> None:
+        self.ip = ip
+        self.port = port
+        self.integrated_addr = (self.ip, self.port) # Attention! This is for computer itself!
+        self.thread_receive = threading.Thread(None, self.receive, "receive")
+        self.thread_deliver = threading.Thread(None, self.deliver, "deliver", "a")
+    
+    def connect(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.connect(self.integrated_addr)
+        print(f"Connected successfully!")
+        self.thread_receive.start()
+        self.thread_deliver.start()
+        
+    def receive(self):
+        while True:
+            recv_data = self.s.recv(1024).decode("utf-8")
+            print(recv_data)
+               
+    def deliver(self,
+                context:str):
+        while True:
+            context = input("Send:")
+            self.s.send(context.encode("utf-8"))
+    
+if __name__ == "__main__":
+    tcp_drone = TCP_Drone("10.11.195.104", 5199)
+    tcp_drone.connect()
